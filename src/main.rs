dcedf7c07f8b6bb62ea9ef9f11da56f08367884d
Rolen T. Le <rolentle@gmail.com>
2022-01-16 15:27:03 -0500

    basic game cycle

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,33 @@ fn main() {
     let answer = ANSWERS[0];
         println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+    let mut turn: u8 = 1;
+    
+    while turn < 7 {
+        let mut guess = String::new();
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+        println!("guess: {}", &guess);
 
-    let guess: String = guess.trim().parse().expect("Please type in a word");
-    println!("guess: {}", &guess);
-    let valid: bool = VALID_WORDS
-        .iter()
-        .map(|w| w.to_string())
-        .collect::<String>()
-        .contains(&guess);
-    println!("valid word: {}", valid);
-    println!("answer: {}", answer);
-    println!("correct?: {}", answer == guess);
+        let guess: String = guess.trim().parse().expect("Please type in a word");
+        let is_valid_word: bool = VALID_WORDS
+            .iter()
+            .map(|w| w.to_string())
+            .collect::<String>()
+            .contains(&guess);
+        if is_valid_word {
+            if answer == guess {
+                println!("{} is {}",answer, guess);
+                break
+            } else {
+                turn += 1;
+                println!("{} is incorrect", guess);
+            }
+        } else {
+            turn += 1;
+            println!("{} is invalid", guess);
+        };
+    }
+    println!("Gameover!");
 }
