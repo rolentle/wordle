c72a8318f48872c5b9608da6bab7e5adab606a04
Rolen T. Le <rolentle@gmail.com>
2022-01-16 14:52:34 -0500

    validate words

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+use std::io;
 mod words;
-use crate::words::ANSWERS;
+use crate::words::*;
 
 fn main() {
     let answer = ANSWERS[0];
-    println!("{}", answer);
+        println!("Please input your guess.");
+
+    let mut guess = String::new();
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+
+    let guess: String = guess.trim().parse().expect("Please type in a word");
+    println!("guess: {}", &guess);
+    let valid: bool = VALID_WORDS
+        .iter()
+        .map(|w| w.to_string())
+        .collect::<String>()
+        .contains(&guess);
+    println!("valid word: {}", valid);
+    println!("answer: {}", answer);
+    println!("correct?: {}", answer == guess);
 }
